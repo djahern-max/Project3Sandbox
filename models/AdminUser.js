e11e1ce965ec5768be5065aae2fccc2b746790b9
Dane Ahern <danielaherniv@gmail.com>
2019-11-30 15:54:38 -0500

    setting up admin user route

---
+++ models/AdminUser.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const adminUserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = adminUser = mongoose.model('adminUser', adminUserSchema);
--- server.js
+++ server.js
@@ -6,7 +6,20 @@ const app = express();
 // Connect Database
 connectDB();
 
+// Init Middleware
+app.use(
+  express.json({
+    extended: false
+  })
+);
+
 app.get('/', (req, res) => res.send('API Running'));
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/adminUsers', require('./routes/api/adminUsers'));
 
 const PORT = process.env.PORT || 8000;
 
